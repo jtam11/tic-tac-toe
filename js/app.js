e4d4052235b2a5084c77671135d677e6731346f2
jtam11 <justinktam@gmail.com>
2016-03-20 19:46:25 -0700

    Able to switch players. Updated CSS to change colors of X's and O's.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,29 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  var player = 'x';
+  var player = "X";
+
+  function nextPlayer() {
+    if (player === "X") {
+      player = "O";
+    } else {
+      player = "X";
+    }
+  }
 
   $('.box').on('click', function placeIcon() {
     $(this).text(player);
+    $(this).addClass(player);
+    nextPlayer();
   });
 
+
+
   $('#reset').on('click', function resetBoard() {
-    $('.box').text('');
+    $('.box').text("");
+    $('.box').removeClass("X");
+    $('.box').removeClass("O");
+    player = "X";
   });
 
 });
